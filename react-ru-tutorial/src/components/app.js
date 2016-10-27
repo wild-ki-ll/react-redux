320003b1f52ad43659ed9a81f7f68ea069eb71de
kirill <kirill@eldis.ru>
2016-10-27 15:00:28 +0700

    Использование props

--- react-ru-tutorial/src/components/app.js
+++ react-ru-tutorial/src/components/app.js
@@ -1,11 +1,11 @@
-import React    from 'react'
-import News     from './news'
-import Comments from './comment'
+import React            from 'react'
+import News, {my_news}  from './news'
+import Comments         from './comment'
 
 let App = () => (
     <div className="app">
         Всем привет, я компонент App! Я умею отображать новости.
-        <News />
+        <News data={my_news} />
         <Comments />
     </div>
 )
--- react-ru-tutorial/src/components/news.js
+++ react-ru-tutorial/src/components/news.js
@@ -1,9 +1,34 @@
 import React from 'react'
 
-const News = () => (
-  <div className="news">
-    К сожалению, новостей нет.
-  </div>
-)
+export const my_news = [
+  {
+    author: 'Саша Печкин',
+    text: 'В четверг, четвертого числа...'
+  },
+  {
+    author: 'Просто Вася',
+    text: 'Считаю, что $ должен стоить 35 рублей!'
+  },
+  {
+    author: 'Гость',
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+  }
+];
+
+const News = (newsList) => {
+    let tmp = newsList.data.map(
+        ({author, text}, index) => 
+            <div key={index}>
+                <p className="news_author">{author}:</p>
+                <p className="news_text">  {text}</p>
+            </div> 
+    )
+
+    return (
+        <div className="news_list"> 
+            {tmp}
+        </div>
+    )  
+}
 
 export default News
